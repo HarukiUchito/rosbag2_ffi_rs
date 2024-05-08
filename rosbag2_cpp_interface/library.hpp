04a6b7d770706af2fbb4b9a673e6e2e3734d2f20
Haruki Uchiito <uhcahriuiktio0114@outlook.com>
2024-05-08 23:47:53 +0900

    apply clang format

--- rosbag2_cpp_interface/library.hpp
+++ rosbag2_cpp_interface/library.hpp
@@ -2,32 +2,35 @@
 #define CMAKE_LIBRARY_H
 #include <rosbag2_cpp/reader.hpp>
 
-typedef struct {
+typedef struct
+{
     rosbag2_cpp::Reader impl;
     std::vector<rosbag2_storage::TopicMetadata> metadata;
 } Rosbag2CppReaderImpl;
 
-typedef struct {
-    const char* topic_name;
+typedef struct
+{
+    const char *topic_name;
     size_t topic_buffer_size;
-    const uint8_t* topic_buffer;
+    const uint8_t *topic_buffer;
 } Rosbag2Topic;
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-Rosbag2CppReaderImpl* create_bag_reader();
-void open_bag(Rosbag2CppReaderImpl* reader, const char* filepath);
+    Rosbag2CppReaderImpl *create_bag_reader();
+    void open_bag(Rosbag2CppReaderImpl *reader, const char *filepath);
 
-size_t get_number_of_topics(Rosbag2CppReaderImpl* reader);
-const char* get_topic_name(Rosbag2CppReaderImpl* reader, size_t num);
-const char* get_topic_type(Rosbag2CppReaderImpl* reader, size_t num);
+    size_t get_number_of_topics(Rosbag2CppReaderImpl *reader);
+    const char *get_topic_name(Rosbag2CppReaderImpl *reader, size_t num);
+    const char *get_topic_type(Rosbag2CppReaderImpl *reader, size_t num);
 
-bool has_next_topic(Rosbag2CppReaderImpl*);
-Rosbag2Topic get_next_topic(Rosbag2CppReaderImpl* reader);
+    bool has_next_topic(Rosbag2CppReaderImpl *);
+    Rosbag2Topic get_next_topic(Rosbag2CppReaderImpl *reader);
 
-void close_bag(Rosbag2CppReaderImpl* reader);
+    void close_bag(Rosbag2CppReaderImpl *reader);
 
 #ifdef __cplusplus
 }
